567995bec6396bc84c0219ad6466b6b5a6a762b9
PJ Frias <peejfrias@gmail.com>
2018-02-20 23:17:58 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,5 @@
 class SongsController < ApplicationController
-  before_action :set_song, only: [:show, :new, :edit, :update, :delete]
+  before_action :set_song, only: [:show, :new, :edit, :update, :destroy]
 
   def index
     @songs = Song.all
